136898461d3bfe45193e618dbeb6790500bad453
Diego Padilla <61509950+dieegopa@users.noreply.github.com>
2021-09-20 14:19:33 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAf1wbR5e3c4n6w5n7gfG2pfsdmkg3pNfo",
-    authDomain: "redux-react-udemy-dc713.firebaseapp.com",
-    projectId: "redux-react-udemy-dc713",
-    storageBucket: "redux-react-udemy-dc713.appspot.com",
-    messagingSenderId: "348143266827",
-    appId: "1:348143266827:web:4936ae53faca7a8020ab0e"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
